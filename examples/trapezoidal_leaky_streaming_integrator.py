ca4542b2ff51ba421784a9372c585e2d5aafda39
Pavel Kirienko <pavel.kirienko@gmail.com>
2026-06-01 13:54:05 +0300

    Examples YAGNI

--- examples/trapezoidal_leaky_streaming_integrator.py
+++ examples/trapezoidal_leaky_streaming_integrator.py
@@ -1,16 +1,6 @@
 class TrapezoidalLeakyStreamingIntegrator:
-    """
-    Classes can be used to define stateful modules.
-    The __init__ method defines the initial states acquired at module reset; all of these must resolve to constant
-    expressions upon constant folding OR be derived from __init__ keyword-only scalar parameters with defaults,
-    which map to elaboration-time parameters.
-    """
-
-    def __init__(self, *, K: float = 2**-22) -> None:
-        """
-        K translates to `parameter real K = 2**-22`.
-        """
-        self.k: float = K
+    def __init__(self, *, k: float = 2**-22) -> None:
+        self.k: float = k
         self.y: float = 0.0
         self._x_prev: float = 0.0
 
